0a6a86e6f02f759e4fca7f48cd3c15042e108834
reomoon <taehyung-moon@nhnservice.com>
2024-12-23 17:41:51 +0900

    v1.16 Order Promotion 추가

--- Lib/common_utils.py
+++ Lib/common_utils.py
@@ -61,7 +61,61 @@ def checkout_process(page):
 
     # 주문 완료 후 Thank you for your order! 텍스트가 포함된 h2 요소 확인
     page.wait_for_load_state()  # 페이지가 완전히 로드될 때까지 기다리기
-    if page.locator('h2.order-title').is_visible():  # h2 태그의 order-title 클래스가 있으면 성공
+    if page.locator('h2.order-title').count() > 0:  # h2 태그의 order-title 클래스가 1개 이상 있으면 성공
+        print("Order successful! Test passed.")
+    else:
+        print("Order not found! Test failed.")
+
+def checkout_promotion(page):
+    """
+    Cart 부터 시작
+    """
+    # Shopping BAG 클릭 후 URL 검증
+    expected_url = 'https://dev-www.fashiongo.net/cart'
+    page.wait_for_url(expected_url)
+
+    assert page.url == expected_url, f"Fail: Expected URL {expected_url}, but got {page.url}." # assert를 사용하여 실패 시 메시지 출력
+    print(f"Success: {expected_url} matched the expected value!")
+
+    # Cart > Select Vendor Promotions 버튼 클릭(Vendor ID 16502 Allium)
+    page.click_locator('button.btn-vendor.size-medium_blue[data-nclick-extra*="vid=16502"]')
+    # 60% Off & Free Shipping $50.00+ Orders
+    apply_button = page.locator('button.btn-apply.nclick', has_text="Apply").nth(0) # 첫 번째 버튼 클릭
+    apply_button.click()
+
+    # Cart > Proceed To Checkout 버튼 클릭
+    page.click_locator('.btn-dark_grey.btn-checkoutAll.nclick')
+
+    # You Have Promotions! 팝업
+    # page.locator('button.btn-sure', has_text="Continue To Checkout").click()
+
+    '''
+    Checkout Step1_Shipping
+    '''
+    # Save & Continue 버튼 클릭
+    page.locator('.btn-dark_grey.btn-goToPayment').click()
+
+    ## Verify Your Address 팝업
+    page.locator('.common-btn.c-black', has_text="Keep This Address").click()
+
+    '''
+    Checkout Step2_Payment
+    '''
+    # Save & Continue 버튼 클릭
+    page.click_locator('.btn-dark_grey.btn-goToReview')
+
+    '''
+    Checkout Step3_Order Review
+    '''
+    # Submit Order 버튼 클릭
+    page.click_locator('.btn-dark_grey.btn-checkout')
+
+    # 잠시 대기 (3초 대기)
+    page.wait_for_timeout(3000)
+
+    # 주문 완료 후 Thank you for your order! 텍스트가 포함된 h2 요소 확인
+    page.wait_for_load_state()  # 페이지가 완전히 로드될 때까지 기다리기
+    if page.locator('h2.order-title').count() > 0:  # h2 태그의 order-title 클래스 1개 이상 있으면 성공
         print("Order successful! Test passed.")
     else:
         print("Order not found! Test failed.")
--- tests/test_orders.py
+++ tests/test_orders.py
@@ -1,6 +1,7 @@
 import pytest
 from Pages.order_openpack_page import order_openpack
 from Pages.order_prepack_page import order_prepack
+from Pages.order_promotion_page import order_promotion
 from test_login import login_fixture
 
 def test_order_openpack(login_fixture):
@@ -11,5 +12,9 @@ def test_order_openpack(login_fixture):
 def test_order_prepack(login_fixture):
     page = login_fixture    # 로그인된 페이지 사용
     order_prepack(page)     # order_prepack 실행
+
+def test_order_promotion(login_fixture):
+    page = login_fixture
+    order_promotion(page)
 
    
